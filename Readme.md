d487b2e3c91942b6847e261fe85bd5b9a93a3939
crimsen <tim@groeger-clan.de>
2018-08-13 22:07:29 +0200

    added Singleton, so PasswordSafe can only run once at a time

--- Readme.md
+++ Readme.md
@@ -43,6 +43,7 @@ We provide a script to install PasswordSafe:
 * tk
 * python-tk
 * python-enum34 (when using python2)
+* tendo
 
 ##### Archlinux:
 
--- src/PasswordSafe.py
+++ src/PasswordSafe.py
@@ -5,9 +5,14 @@ Created on 27.03.2015
 
 @author: crimsen
 '''
+
+from tendo import singleton
 from controller.MainController import MainController
+
+
 
 if __name__ == '__main__':
     
+    me = singleton.SingleInstance()
     App = MainController()
     App.show()
